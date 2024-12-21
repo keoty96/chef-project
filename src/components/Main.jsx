492e3f798f3d9c4424068f7b2b6538c5a411bae1
keoty96 <kosorio457@gmail.com>
2024-12-21 14:50:13 -0500

    props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => {
+        return (
+            <li key={ingredient}>{ingredient}</li>
+        )
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Form submitted");
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient);
+        console.log(ingredients)
+    }
+
     return(
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                 type="text"
                 placeholder="e.g. oregano"
                 aria-label="Add ingredient"
+                name="ingredient"
                 />
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+            {ingredientsListItems}
+            </ul>
         </main>
     )
 }
