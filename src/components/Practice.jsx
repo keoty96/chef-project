aad2f913b34ff6b024a83e97ea228b96606fddad
keoty96 <kosorio457@gmail.com>
2024-12-25 14:20:18 -0500

    add get recipe and conditionals

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export default function Practice() {
 
     const [count , setCount] = useState(0);
+    const [messages, setMessages] = useState(['a', 'b', 'c']);
 
     function add() {
         setCount(prevCount => prevCount +  1)
@@ -12,11 +13,25 @@ export default function Practice() {
         setCount(prevCount => prevCount - 1)
     }
 
+    function checkMessages() {
+        if (messages.length === 0) {
+            return "You're all caught up"
+        } else if (messages.length === 1) {
+            return "You have 1 unread messages"
+        } else {
+            return `You have ${messages.length} unread messages`
+        }
+    }
+
+    checkMessages();
+
     return (
         <>
             <button onClick={substract}>-</button>
             <div>{count}</div>
             <button onClick={add}>+</button>
+            <hr />
+            <h2>{checkMessages()}</h2>
         </>
     )
 }
